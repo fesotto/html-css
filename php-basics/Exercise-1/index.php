e9caf9c03f19beeca03d4ce371a36a884fd098cd
Jeffe Otto <fesotto@yahoo.es>
2022-02-28 12:19:30 +0100

    Php Basics Exercise 1

---
+++ php-basics/Exercise-1/index.php
@@ -0,0 +1,19 @@
+<h1 style="text-align:center; margin:30px;">Exercises PHP Nivel 1</h1>
+
+<?php 
+$a_integer = 5;
+$a_double = 45.34;
+$a_string = "String Example";
+$a_boolean = true;
+
+echo "Integer Variable: " . $a_integer . '<br>' ;
+
+echo "Double Variable: " . $a_double . '<br>' ;
+
+echo "String Variable: " . $a_string . '<br>' ;
+
+echo "Boolean True Variable: " . $a_boolean . '<br>' ;
+
+?>
+
+
