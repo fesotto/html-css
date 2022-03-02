c416bc59a786d038f0739ddecacc649cb234c24c
Jeffe Otto <fesotto@yahoo.es>
2022-03-02 11:00:00 +0100

    Level 2 exercise 2

--- php-basics/nivel-2/index.php
+++ php-basics/nivel-2/index.php
@@ -11,3 +11,23 @@ function sum($x,$y)
     echo ($x == $y) ? ($x + $y) * 2 : $x + $y;
  
 }
+
+//Exercise -1
+echo "Exercise -2";
+
+
+function replace($string)
+{ 
+    $first = $string[0];
+    $last = substr($string, -1, 1);
+   
+    if(strlen($string) > 1){
+      $string = substr_replace($string, $first, -1 ,strlen($string));
+      $string = substr_replace($string, $last, 0 ,1);
+      
+      echo $string;
+    }else{
+        echo $string;
+    }
+    
+} 
