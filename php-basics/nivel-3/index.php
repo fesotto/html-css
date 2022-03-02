3aa48146c271852500ac33dd4707445b9eca376a
Jeffe Otto <fesotto@yahoo.es>
2022-03-02 13:08:10 +0100

    Level 3 exercise 2

--- php-basics/nivel-3/index.php
+++ php-basics/nivel-3/index.php
@@ -7,11 +7,33 @@
 echo "Exercise -1";
 
 //Convert a tring to an array without blank spaces
-function stringToArray($string)
-{
+function stringToArray($string){
       $string = str_replace(' ', '', $string);
       var_dump(str_split($string));
 }
-stringToArray("any sentence will be an array with no spaces between letters");
+//stringToArray("any sentence will be an array with no spaces between letters");
 
+echo '<br>';
+echo '<br>';
+
+
+//Exercise -2
+echo "Exercise -2";
+echo '<br>';
+
+function repeate_values($array){
+   $values = array_count_values($array);
+       foreach ($values  as $key => $val) {
+        $string_times = "times";
+             if($val < 2){
+               $string_times = "time";
+             }
+              
+        echo "$key apperas $val $string_times in the array.<br>" ;    
+      }
+}
+$my_array = array(12,43,66,21,56,43,43,78,78,100,43,43,43,21);
+repeate_values($my_array);
+echo '<br>';
+echo '<br>';
 
