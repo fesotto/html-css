2b5c1abf441627b96bb05503685daa934dc9a8b5
Jeffe Otto <fesotto@yahoo.es>
2022-03-08 12:23:26 +0100

    OOP Level 2 exercise and folder

---
+++ oop/nivel-2/index.php
@@ -0,0 +1,45 @@
+
+
+<?php
+
+class PokerDice{
+    
+    private $dice = ["A","K","Q","A","7","8"];
+    private $thrwonDice;    
+    private $face;
+    private static $totalThrows = 0;
+    private $totalTime;
+
+
+    private function throw()
+    {
+       return $this->thrwonDice = array_rand($this->dice);   
+    }
+    public function shapeName()
+    {
+        echo $face = $this->dice[$this->throw()];
+    }
+
+    private function throwNtimes()
+    { 
+            $dice = new self();
+
+            for($i = 0 ; $i < 5; $i++){
+             $this->totalThrows[$i] = $dice->throw();
+             self::$totalThrows++;
+            }
+
+            return self::$totalThrows;
+           
+    }
+
+    public  function  getTotalThrows()
+    {
+       return $this->throwNtimes();
+    }
+
+}
+
+
+
+
