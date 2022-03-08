4c53e619d19f1af1523d79b215dcf6aa4516d08b
Jeffe Otto <fesotto@yahoo.es>
2022-03-08 18:29:24 +0100

    Create Account Class

---
+++ oop/nivel-3/classes/Account.php
@@ -0,0 +1,82 @@
+<?php
+
+include('Message.php');
+class Account extends Message 
+{
+    private $name;
+    private $givenName;
+    private $balance;
+    private $error = " ";
+
+    /*
+       For testing withdraw and deposit only let name and givenName as default empty
+       to avoid errors in class initialization
+    */
+    public function __construct($name = " ", $givenName = " ")
+    {
+        $this->name = $name;
+        $this->givenName = $givenName;
+        $this->balance = 0;
+    }
+
+   public function withdraw($amount)
+    {
+        
+        $this->message("Valor de reintegro ${$amount}.", 'success');
+        $this->balance -= $amount;
+    }
+
+    public function deposit($amount)
+    {
+        $this->message("Desposito del valor ${$amount} realizado correctamente.", 'success');
+        return $this->balance += $amount;
+    }
+
+  
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    public function getName()
+    {
+        return $this->name + $this->givenName;
+    }
+
+
+    public function validateWidthdraw($value)
+    { 
+        //repeated code
+        if(!isset($value) || trim($value) === ''){
+        return  $this->message("Por favor introduce un valor", 'warning'); 
+          }
+        
+        if ($value > $this->balance) {
+            $this->message('No funds.','warning');
+            return;
+        }
+
+
+         $this->withdraw($value);
+        
+    }
+    
+
+    public function validateDeposit($value)
+    {
+         //repeated code
+        if(!isset($value) || trim($value) === ''){
+            return  $this->message("Por favor introduce un valor", 'warning'); 
+       }
+       
+        if ($value <= 0) {
+            $this->message('Cantidad tiene que ser maior que 0.', 'warning');
+            return;
+         }
+
+         $this->deposit($value);
+        
+    }
+
+    
+}
